e9dd45e450aa5bc051bc38a9c485be1034181a93
Robert Cruz <robertcruzsm@gmail.com>
2026-03-12 18:32:50 -0400

    Diretórios e dependências estruturadas

---
+++ backend/Full-Bicho/src/main/java/com/RobbyDevs/Full_Bicho/FullBichoApplication.java
@@ -0,0 +1,13 @@
+package com.RobbyDevs.FullBicho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullBichoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullBichoApplication.class, args);
+	}
+
+}
---
+++ backend/Full-Bicho/src/test/java/com/RobbyDevs/Full_Bicho/FullBichoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RobbyDevs.Full_Bicho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullBichoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
